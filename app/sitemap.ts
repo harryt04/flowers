ba6b91659bb0cd54bc090e4a90dbc34d1880c059
Harry Thomas <hsthomas@verisk.com>
2026-05-29 22:28:24 -0600

    Update sitemap URL to point to the correct domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://justbecauseflowers.com",
+      url: "https://flowers.harryt.dev",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
